8caeab856a7a469cc112dcab7021d30a12189c41
Filomène Roquefort <filomene.roquefort@gmail.com>
2024-01-08 16:38:31 +0100

    disparition du bouton START mais réapparition lorsqu'on clique sur soumettre

--- static/Script.js
+++ static/Script.js
@@ -12,18 +12,31 @@ function envoyerRequete() {
     xhr.send();
 }
 
+// Variable pour suivre si le bouton START a été cliqué
+var startButtonClicked = false;
+
 function envoyerRequeteStart() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/executer_fonction_python_start", true);
+    // Vérifiez si le bouton a déjà été cliqué
+    if (!startButtonClicked) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/executer_fonction_python_start", true);
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            // Mettez à jour le contenu de la grille avec la réponse reçue du serveur
-            document.getElementById('message').innerHTML = xhr.responseText;
-        }
-    };
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                // Mettez à jour le contenu de la grille avec la réponse reçue du serveur
+                document.getElementById('message').innerHTML = xhr.responseText;
 
-    xhr.send();
+                // Masquer le bouton après avoir reçu la réponse
+                var startButton = document.getElementById("test");
+                startButton.style.display = "none";
+
+                // Mettez à jour la variable pour indiquer que le bouton a été cliqué
+                startButtonClicked = true;
+            }
+        };
+
+        xhr.send();
+    }
 }
 
 function envoyerBuzz() {
